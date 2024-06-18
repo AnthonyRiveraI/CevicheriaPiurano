8900edf7d2a07489c3abf3b7919a17caab443851
AfkunG <anthony.g.rivera.i@gmail.com>
2024-06-18 13:38:37 -0500

    Migracion del proyecto de Ant a Maven

---
+++ src/main/java/restaurante/Restaurante.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package restaurante;
+
+import Vista.FrmLogin;
+
+/**
+ *
+ * @author AfkunGT
+ */
+public class Restaurante {
+
+    public static void main(String[] args) {
+        FrmLogin iniciar = new FrmLogin();
+        iniciar.setVisible(true);
+    }
+}
